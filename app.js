96612f65f2488ecbdeb97932586b945bd732fe17
Yan Kairalla <yanmkairalla@hotmail.com>
2019-09-22 17:14:02 -0300

    added create, download and upload files

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+//The local folder to store and renaming the files 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+	}
+});
+
+//the types accepted to upload or download the images (png, jpg, jpeg)
+const fileFilter = (req, file, cb) => {
+	if(
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.nimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // aplication/json
 
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
+// SETTINGS HEADER 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // accepts access from all ips(url) or writing the url within commas (if necessary)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // the methods allowed;
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
     next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	res.status(status).json({ message });
+})
+
+mongoose.connect(
+	'mongodb+srv://bansaidol:12345@cluster0-9ozdd.mongodb.net/messages'
+	)
+.then(() => {
+	app.listen(8080);	
+})
+.catch(console.log)
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,24 +1,84 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/Post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                title: 'First Post',
-                content: 'this is the first post!'
-            }
-        ]
+    Post.find()
+    .then(posts => {
+        res.status(200).json({
+            message: 'Fetched post successfully',
+            posts
+        });
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+           err.statusCode = 500; 
+        }
+        next(err);
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, data is incorrect!');
+        error.statusCode = 422;
+        throw error;
+    }
+    if(!req.file) {
+        const error = new Error('No image provided');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
-    // Create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            id: new Date().toISOString(),
+    const post = new Post({
             title,
-            content
+            content,
+            imageUrl,
+            creator: {
+                name: 'Yan Kairalla'
+            }  
+    });
+    post.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post  : result 
+        }); 
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+           err.statusCode = 500; 
         }
+        next(err);
+    });
+    // Create post in db
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        console.log(post);
+        res.status(200)
+        .json({
+            message: 'Post fetched',
+             post
+        });
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+           err.statusCode = 500; 
+        }
+        next(err);
     });
 }
