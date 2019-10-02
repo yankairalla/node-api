e16890bb62053a2353c256081f05793a2e45058b
Yan Kairalla <yanmkairalla@hotmail.com>
2019-10-02 16:44:28 -0300

    added socket.io changes

--- app.js
+++ app.js
@@ -62,6 +62,10 @@ mongoose.connect(
 	'mongodb+srv://bansaidol:12345@cluster0-9ozdd.mongodb.net/messages'
 	)
 .then(() => {
-	app.listen(8080);	
+	const server = app.listen(8080);
+	const io = require('./socket').init(server);
+	io.on('connection', socket => {
+		console.log('Client connected!');
+	});
 })
 .catch(console.log)
